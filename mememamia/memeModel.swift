bb8042d447364926c6dd019a94d0561c3ef4545d
timurridjanovic <timur.ridjanovic@gmail.com>
2016-03-18 13:43:03 -0400

    removed init from struct

--- mememamia/memeModel.swift
+++ mememamia/memeModel.swift
@@ -14,11 +14,4 @@ struct Meme {
     var bottomText: String?
     var image: UIImage?
     var memedImage: UIImage?
-    
-    init(topText: String, bottomText: String, image: UIImage, memedImage: UIImage) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.image = image
-        self.memedImage = memedImage
-    }
 }
