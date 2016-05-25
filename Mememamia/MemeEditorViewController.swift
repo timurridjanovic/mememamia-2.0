da493c4799705abed0a4ac9a2b6023a4332c6e97
timurridjanovic <timur.ridjanovic@gmail.com>
2016-05-25 16:05:32 -0400

    reverted selecter syntax from swift 2.2

--- Mememamia/MemeEditorViewController.swift
+++ Mememamia/MemeEditorViewController.swift
@@ -296,12 +296,12 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     }
     
     func subscribeToKeyboardNotification() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func subscribeToOrientationChange() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorViewController.updateTextFields(_:)), name: UIDeviceOrientationDidChangeNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "updateTextFields:", name: UIDeviceOrientationDidChangeNotification, object: nil)
     }
     
     func unsubsribeFromKeyboardNotification() {
